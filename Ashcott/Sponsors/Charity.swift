ce14e6996a0873ea11b7ff902ff1d3e2565ef6d1
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2026-06-07 11:06:55 +0100

    Added doc for Charities

--- Ashcott/Sponsors/Charity.swift
+++ Ashcott/Sponsors/Charity.swift
@@ -17,10 +17,18 @@ nonisolated struct CharityDetails: Codable {
         var id: UUID { UUID() }
         var displayDetail: LocalizedStringResource { "\(detail)" }
     }
-
+    
+    /// A property that shows the main title of ``SponsorView``
     var mainTitle: String
-    private var description: String
-    var charities: [Charity]
 
+    /// A property that show a description of Charities
+    private var description: String
+
+    /// An array of charities
+    var charities: [Charity]
+    
+    /// A property that holds ``description`` of the Charities
+    ///
+    /// - Note: This converts text to display as markdown
     var displayDescription: LocalizedStringResource { "\(description)" }
 }
