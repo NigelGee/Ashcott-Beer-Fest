91b197efe0b3316f6497217374200583bb69fe9c
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2026-08-11 15:17:41 +0100

    Add cache to AsyncImahes

---
+++ Ashcott/Supporting/CacheAsyncImage.swift
@@ -0,0 +1,18 @@
+//
+//  CacheAsyncImage.swift
+//  Ashcott
+//
+//  Created by Nigel Gee on 11/08/2026.
+//
+
+import SwiftUI
+
+struct CacheAsyncImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CacheAsyncImage()
+}
---
+++ Ashcott/Supporting/ImageCache.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCache.swift
+//  Ashcott
+//
+//  Created by Nigel Gee on 11/08/2026.
+//
+
+import Foundation
